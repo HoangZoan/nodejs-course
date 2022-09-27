ca2397e7f61502400350bb220a3cdc5c3f60b09f
HoangZoan <d.hoang1906@gmail.com>
2022-09-27 22:57:47 +0700

    Update: On Sequelize section

--- 6. Sequelize/controllers/admin.js
+++ 6. Sequelize/controllers/admin.js
@@ -13,14 +13,45 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect("/");
+  req.user
+    .createProduct({
+      title,
+      price,
+      imageUrl,
+      description,
+      userId: req.user.id,
+    })
+    .then((result) => {
+      // console.log(result);
+      res.redirect("/admin/products");
+      console.log("Product created");
     })
     .catch((err) => console.log(err));
-  res.redirect("/");
+
+  // ----
+  // Product.create({
+  //   title,
+  //   price,
+  //   imageUrl,
+  //   description,
+  //   userId: req.user.id,
+  // })
+  //   .then((result) => {
+  //     // console.log(result);
+  //     res.redirect("/admin/products");
+  //     console.log("Product created");
+  //   })
+  //   .catch((err) => console.log(err));
+
+  // ----
+  // const product = new Product(null, title, imageUrl, description, price);
+  // product
+  //   .save()
+  //   .then(() => {
+  //     res.redirect("/");
+  //   })
+  //   .catch((err) => console.log(err));
+  // res.redirect("/");
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -29,18 +60,34 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    if (!product) {
-      return res.redirect("/");
-    }
+  req.user
+    .getProducts({
+      where: {
+        id: prodId,
+      },
+    })
+    // Product.findByPk(prodId)
+    .then((products) => {
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: products[0],
+      });
+    })
+    .catch((err) => console.log(err));
+  // Product.findById(prodId, (product) => {
+  //   if (!product) {
+  //     return res.redirect("/");
+  //   }
 
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product,
-    });
-  });
+  //   res.render("admin/edit-product", {
+  //     pageTitle: "Edit Product",
+  //     path: "/admin/edit-product",
+  //     editing: editMode,
+  //     product,
+  //   });
+  // });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -49,29 +96,57 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDescription;
+      return product.save();
+    })
+    .then(() => {
+      res.redirect("/admin/products");
+      console.log("UPDATED PRODUCT");
+    })
+    .catch((err) => console.log(err));
+  // const updatedProduct = new Product(
+  //   prodId,
+  //   updatedTitle,
+  //   updatedImageUrl,
+  //   updatedDescription,
+  //   updatedPrice
+  // );
+  // updatedProduct.save();
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  });
+  req.user
+    .getProducts()
+    // Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
+  // Product.fetchAll((products) => {
+  //   res.render("admin/products", {
+  //     prods: products,
+  //     pageTitle: "Admin Products",
+  //     path: "/admin/products",
+  //   });
+  // });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => product.destroy())
+    .then(() => {
+      res.redirect("/admin/products");
+      console.log("DESTROYED PRODUCT");
+    })
+    .catch((err) => console.log(err));
 };
