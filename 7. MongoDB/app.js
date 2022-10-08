bda0fcd5df250ff78bd74d9e7efbf962b56cc0f6
HoangZoan <d.hoang1906@gmail.com>
2022-10-08 10:22:59 +0700

    Done section 12

--- 7. MongoDB/app.js
+++ 7. MongoDB/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("633c5aa13e15a67071596236")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
